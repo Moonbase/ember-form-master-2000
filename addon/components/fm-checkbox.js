5f1fd1a3610d95170b992b7f7154225682a0cd67
Ward van Teijlingen <w.van.teijlingen@gmail.com>
2015-06-20 16:29:27 +0200

    Added missing parentheses

--- addon/components/fm-checkbox.js
+++ addon/components/fm-checkbox.js
@@ -4,7 +4,7 @@ export default Ember.Component.extend({
   classNameBindings: ['checkboxWrapperClass', 'errorClass'],
   checkboxWrapperClass: Ember.computed(function() {
     return this.fmconfig.checkboxWrapperClass;
-  },
+  }),
   errorClass: Ember.computed('errors', 'showErrors', function() {
     if(this.get('errors') && this.get('showErrors') || this.get('parentView.showErrors')) {
       return this.fmconfig.errorClass;
