77233f03862a87091fc1a59a2e07cd16664a30d6
Ward van Teijlingen <w.van.teijlingen@gmail.com>
2015-06-20 19:15:35 +0200

    Removed extra parentheses

--- addon/mixins/errorable.js
+++ addon/mixins/errorable.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Mixin.create({
   errorClass: Ember.computed('errors', 'showErrors', function() {
     var showErrors = this.get('showErrors') || this.get('parentView.showErrors');
-    if(this.get('errors') && showErrors)) {
+    if(this.get('errors') && showErrors) {
       return this.fmconfig.errorClass;
     }
   })
